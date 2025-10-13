56a6eb82d820448f12cf3d7c88653e0012168de5
phuc <hongphuc.pham.7777@gmail.com>
2025-10-13 21:15:18 +0700

    Fix User Manage for Staff & Manage Staff for Admin

--- src/app/(private)/dashboard/(admin)/layout.tsx
+++ src/app/(private)/dashboard/(admin)/layout.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { ROLE_ADMIN } from "@/constants/constants"
+import { ROLE_ADMIN, ROLE_STAFF } from "@/constants/constants"
 import { useGetMe } from "@/hooks"
 import { Spinner } from "@heroui/react"
 import { useRouter } from "next/navigation"
-import React, { useEffect, useMemo } from "react"
+import React, { useEffect } from "react"
 import toast from "react-hot-toast"
 import { useTranslation } from "react-i18next"
 
@@ -13,20 +13,28 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
     const { t } = useTranslation()
     const { data: user, isLoading, isError } = useGetMe({ enabled: true })
 
-    const isAdmin = useMemo(() => {
-        return user?.role?.name === ROLE_ADMIN
-    }, [user])
+    const roleName = user?.role?.name
+    const isAdmin = roleName === ROLE_ADMIN
+    const isStaff = roleName === ROLE_STAFF
 
     useEffect(() => {
         if (isLoading) return
+
+        if (isStaff) {
+            router.replace("/dashboard/user-management")
+            return
+        }
+
         if (isError || !isAdmin) {
             toast.dismiss()
             toast.error(t("user.unauthorized"))
             router.replace("/")
         }
-    }, [isError, isLoading, isAdmin, router, t])
+    }, [isAdmin, isError, isLoading, isStaff, router, t])
 
     if (isLoading) return <Spinner />
+
+    if (!isAdmin) return null
 
     return <>{children}</>
 }
